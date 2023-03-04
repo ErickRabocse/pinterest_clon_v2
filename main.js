6cb585936aa617690c853223b080c65fba131d34
Erick Chavez <erickchavezescobar@gmail.com>
2023-03-03 21:22:36 -0600

    JS editchanged margin for opacity

--- main.js
+++ main.js
@@ -3,19 +3,22 @@ const image_cover = document.querySelector(".main__card_img_cover");
 const image = document.querySelector(".main__card_img");
 
 function showDisplayCover() {
-  image_cover.style.marginLeft = "0";
+  image_cover.style.opacity = "1";
   image.style.filter = "brightness(60%)";
 }
 
 function hideDisplayCover() {
-  image_cover.style.marginLeft = "1000px";
+  image_cover.style.opacity = "0";
   image.style.filter = "brightness(100%)";
 }
 
 image_box.addEventListener("mouseover", showDisplayCover);
 image_box.addEventListener("mouseout", hideDisplayCover);
 
-// image_box.forEach((el) => {
-//   el.addEventListener("mouseover", showDisplayCover);
-//   el.addEventListener("mouseout", hideDisplayCover);
-// });
+// for (var i = 0; i < image_box.length; i++) {
+//   image_box[i].addEventListener("click", function () {
+//     // Get the index of the clicked element in the array
+//     var index = Array.prototype.indexOf.call(image_box, this);
+//     console.log("Index of clicked image box: " + index);
+//   });
+// }
